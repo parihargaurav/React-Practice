f131045d018dd9d5b604655b1ce80aa2d26806b6
parihargaurav <parihargaurav527@gmail.com>
2024-08-10 17:40:00 +0530

    stepper project

---
+++ stepper/src/App.jsx
@@ -0,0 +1,31 @@
+import "./App.css";
+import CheckoutStepper from "./components/stepper";
+
+const CHECKOUT_STEPS = [
+  {
+    name: "Customer Info",
+    Component: () => <div> Provide your details.</div>,
+  },
+  {
+    name: "Shipping Info",
+    Component: () => <div>Enter your shipping address.</div>,
+  },
+  {
+    name: "Payment",
+    Component: () => <div>Complete payment for your order.</div>,
+  },
+  {
+    name: "Delivered",
+    Component: () => <div> Your order has been delivered.</div>,
+  },
+];
+
+function App() {
+  return (
+    <div>
+      <CheckoutStepper stepsConfig={CHECKOUT_STEPS} />
+    </div>
+  );
+}
+
+export default App;
